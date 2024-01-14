3503b87a3dc25728eb3a3335fd0a4246879c8497
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-14 13:02:06 +0100

    implement forwardRef

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,42 +1,43 @@
 import clsx from 'clsx';
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { UseFormRegister, FieldValues } from 'react-hook-form';
 
 import { InputFieldSize, InputFieldColor, InputFieldVariant, InputFieldProps } from './InputField';
 
-export const InputField = ({
-  size = InputFieldSize.Medium,
-  color = InputFieldColor.Primary,
-  variant = InputFieldVariant.Bordered,
-  onChange,
-  type,
-  placeholder,
-  isDisabled,
-  label
-}: InputFieldProps) => {
+export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps & ReturnType<UseFormRegister<FieldValues>>>(
+  ({
+    size = InputFieldSize.Medium,
+    color = InputFieldColor.Primary,
+    variant = InputFieldVariant.Bordered,
+    onChange,
+    type,
+    placeholder,
+    isDisabled,
+    label
+  }, ref) => {
 
-  const form = useForm();
+    const handleChange = (
+      event: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      console.log(event.target.value);
+    };
 
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    console.log(event.target.value);
-  };
-
-  return (
-    <label className="form-control w-full max-w-xs">
-      {label && (
-        <div className="label">
-          <span className="label-text">{label}</span>
-        </div>
-      )}
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={clsx(`${size} ${color} ${variant} input px-2 h-10`)}
-        disabled={isDisabled}
-        onChange={handleChange}
-      />
-    </label>
-  );
-};
+    return (
+      <label className="form-control w-full max-w-xs">
+        {label && (
+          <div className="label">
+            <span className="label-text">{label}</span>
+          </div>
+        )}
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={clsx(`${size} ${color} ${variant} input px-2 h-10`)}
+          disabled={isDisabled}
+          onChange={handleChange}
+          ref={ref}
+        />
+      </label>
+    );
+  }
+);
