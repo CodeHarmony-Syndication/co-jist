7f24d2003ef362ec4ba77955bd242b1c497e0bf7
Gabriela Biswasová <pracho.dayat@seznam.cz>
2024-02-19 13:11:52 +0100

    textarea cmp 2nd try

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,15 +2,16 @@ import clsx from 'clsx'
 import React from 'react'
 import { TextAreaProps, TextAreaSize ,TextAreaOption } from '../types/type.ts'
 
-export const TextArea =({text, size = TextAreaSize.Medium}
-):TextAreaProps => 
+export const TextArea =({text, placeholder, size = TextAreaSize.Medium}
+) => {
     return(
-    <>
+    <form>
         <label htmlFor="textarea-title" className=""></label>
-        <textarea className={clsx('textarea', size)}>{text}</textarea>
-    </>
+        <textarea placeholder="Napište text" className={clsx('textarea', size)}>{text}</textarea>
+        <button>Odeslat</button>
+    </form>
 )
-
+}
     
 // Create a component for textarea element based on Daisy UI. Include props for different size and style of the textarea.
 
